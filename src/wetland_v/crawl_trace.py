4966b59ca1ef2c758140f21ba921b4d0fef18058
Jaweed Nazary <mjwd1991@gmail.com>
2026-01-30 15:50:13 -0600

    Update crawl_trace.py

--- src/wetland_v/crawl_trace.py
+++ src/wetland_v/crawl_trace.py
@@ -403,7 +403,8 @@ def get_lidar_points(geom_3857: BaseGeometry,
             # Optional tuning knobs:
             #smrf: Optional[Dict] = None,
             #csf: Optional[Dict] = None,
-            #pmf: Optional[Dict] = None,) -> Dict:
+            #pmf: Optional[Dict] = None,
+            ) -> Dict:
 
 
 
